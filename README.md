e9e5d7d090dbbfdcdb55baa2890327c6afbd01f0
Allef Sousa <allefsousaicm1994@gmail.com>
2020-10-25 00:20:12 -0300

    fist version of validation with Vuelidate

--- README.md
+++ README.md
@@ -1,24 +1,3 @@
-# vuelidate
+# Formulário com Vuelidate
 
-## Project setup
-```
-yarn install
-```
-
-### Compiles and hot-reloads for development
-```
-yarn serve
-```
-
-### Compiles and minifies for production
-```
-yarn build
-```
-
-### Lints and fixes files
-```
-yarn lint
-```
-
-### Customize configuration
-See [Configuration Reference](https://cli.vuejs.org/config/).
+Implementação da validação de formulários com Vuejs e Vuelidate
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,183 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="container">
+    <h1>Trabalhando com Formulários</h1>
+    <form
+      @submit.prevent="submit"
+      @reset.prevent="reset"
+      class="form-container"
+    >
+      <div class="input-group">
+        <label for="phone">Telefone:</label>
+        <input
+          type="tel"
+          placeholder="seu numero de celular"
+          v-mask="'(##)#####-####'"
+          v-model.trim.lazy="$v.phone.$model"
+        />
+      </div>
+      <div class="error" v-if="!$v.phone.required && $v.phone.$dirty">
+        Campo obrigatório.
+      </div>
+      <div class="error" v-if="!$v.phone.minLength && $v.phone.$dirty">
+        O telefone deve ter 9 digitos.
+      </div>
+
+      <div class="input-group">
+        <label for="phone">CPF:</label>
+        <input
+          type="num"
+          placeholder="CPF"
+          v-mask="'###.###.###-##'"
+          v-model.trim.lazy="$v.cpf.$model"
+        />
+      </div>
+      <div class="error" v-if="!$v.cpf.required && $v.cpf.$dirty">
+        Campo obrigatório.
+      </div>
+      <div class="error" v-if="!$v.cpf.minLength">
+        O CPF deve ter 11 numeros.
+      </div>
+      <div class="buttons">
+        <button type="submit">enviar</button>
+        <button type="reset">limpar</button>
+      </div>
+      <transition name="fade" mode="out-in">
+        <p class="badge success" v-if="stateForm === 'validRequest'">
+          Tudo certo
+        </p>
+        <p class="badge warning" v-if="stateForm === 'invalidRequest'">
+          Preencha os campos corretamente.
+        </p>
+      </transition>
+    </form>
   </div>
 </template>
 
 <script>
+import { required, minLength } from "vuelidate/lib/validators";
+import { mask } from "vue-the-mask";
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  directives: { mask },
+  name: "vue",
+  data() {
+    return {
+      phone: "",
+      cpf: "",
+      stateForm: null,
+    };
+  },
+  methods: {
+    reset() {
+      (this.phone = ""), (this.cpf = ""), (this.stateForm = null);
+    },
+    submit() {
+      this.$v.$touch();
+      if (this.$v.$invalid) {
+        setTimeout(() => {
+          this.stateForm = "invalidRequest";
+        }, 1000);
+      } else {
+        setTimeout(() => {
+          this.stateForm = "validRequest";
+          (this.phone = ""), (this.cpf = "");
+        }, 1000);
+      }
+    },
+  },
+  validations: {
+    phone: {
+      required,
+      minLength: minLength(13),
+    },
+    cpf: {
+      required,
+      minLength: minLength(14),
+    },
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.container {
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  background: #eee;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.form-container {
+  border: 1px solid black;
+  border-radius: 4px;
+  padding: 20px;
+  width: 40%;
+  display: grid;
+  grid-template-columns: 1fr;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.input-group {
+  display: flex;
+  flex-direction: column;
+  align-content: flex-start;
 }
-a {
-  color: #42b983;
+.input-group label {
+  margin: 5px 0;
+}
+
+.input-group input {
+  padding: 0.6rem;
+  border-radius: 4px;
+  color: #333;
+  border: 1px solid #333;
+}
+
+.error {
+  font-size: 11px;
+  font-style: italic;
+  padding: 5px;
+}
+button:nth-child(1) {
+  margin-right: 15px;
+}
+button {
+  padding: 0.6rem;
+  color: #eee;
+  background: #432d92;
+  border: 1px solid #342374;
+  border-radius: 4px;
+  font-size: 16px;
+  margin: 20px 0 0;
+  cursor: pointer;
+  transition: all 0.2s;
+  text-transform: uppercase;
+}
+button:hover {
+  background: #342374;
+  border: 1px solid #432d92;
+}
+
+.badge {
+  border: 1px solid;
+  border-radius: 4px;
+  padding: 5px;
+  text-align: center;
+  color: #eee;
+}
+.success {
+  background: #13972985;
+}
+.warning {
+  background: #b62d288a;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s;
+}
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
